1b53194d5301e810f54915f262047512a0a99880
Subhajit Patra <1souvikpa@gmail.com>
2020-10-03 20:17:07 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char first;
+    printf("input first letter.\n");
+    scanf("%c",&first);
+    switch(first){
+    case 'A':
+        printf("aged actor.\n");
+        break;
+    case 'B':
+        printf("big basket.\n");
+        break;
+    case 'C':
+        printf("costly car.\n");
+        break;
+    case 'D':
+        printf("dark chocolate.\n");
+        break;
+    case 'E':
+        printf("edible egg.\n");
+        break;
+    case 'F':
+        printf("fast food.\n");
+        break;
+    case 'G':
+        printf("glamorous.\n");
+        break;
+    case 'H':
+        printf("haunted house.\n");
+        break;
+    case 'I':
+        printf("icy cool.\n");
+        break;
+    case 'J':
+        printf("jealous courtiers.\n");
+        break;
+    case 'K':
+        printf("keen eyes.\n");
+        break;
+    case 'L':
+        printf("last warning.\n");
+        break;
+    case 'M':
+        printf("mad bull.\n");
+        break;
+    case 'N':
+        printf("native soldiers.\n");
+        break;
+    case 'O':
+        printf("obedient student.\n");
+        break;
+    case 'P':
+        printf("playful panda.\n");
+        break;
+    case 'Q':
+        printf("quarrelsome people.\n");
+        break;
+    case 'R':
+        printf("rapid spin.\n");
+        break;
+    case 'S':
+        printf("sad face.\n");
+        break;
+    case 'T':
+        printf("tricky question.\n");
+        break;
+    case 'U':
+        printf("ugly cockroach.\n");
+        break;
+    case 'V':
+        printf("virtual classroom.\n");
+        break;
+    case 'W':
+        printf("warm climate.\n");
+        break;
+    case 'X':
+        printf("xerophytic plant.\n");
+        break;
+    case 'Y':
+        printf("yellow color.\n");
+        break;
+    case 'Z':
+        printf("zigzag road.\n");
+        break;
+    default:
+        printf("invalid.\n");
+
+    }
+    return 0;
+}
