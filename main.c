007772198c2440a4d43b3bab13e59646be276cfc
Subhajit Patra <1souvikpa@gmail.com>
2020-10-05 21:24:49 +0530

    two characters support added.

--- main.c
+++ main.c
@@ -3,91 +3,230 @@
 
 int main()
 {
-    char first;
+    char first,second;
     printf("input first letter.\n");
     scanf("%c",&first);
+    printf("input second letter.\n");
+    scanf("%c",&second);
+
     switch(first){
     case 'A':
+    case 'a':
         printf("aged actor.\n");
         break;
     case 'B':
+    case 'b':
         printf("big basket.\n");
         break;
     case 'C':
+    case 'c':
         printf("costly car.\n");
         break;
     case 'D':
+    case 'd':
         printf("dark chocolate.\n");
         break;
     case 'E':
+    case 'e':
         printf("edible egg.\n");
         break;
     case 'F':
+    case 'f':
         printf("fast food.\n");
         break;
     case 'G':
+    case 'g':
         printf("glamorous.\n");
         break;
     case 'H':
+    case 'h':
         printf("haunted house.\n");
         break;
     case 'I':
+    case 'i':
         printf("icy cool.\n");
         break;
     case 'J':
+    case 'j':
         printf("jealous courtiers.\n");
         break;
     case 'K':
+    case 'k':
         printf("keen eyes.\n");
         break;
     case 'L':
+    case 'l':
         printf("last warning.\n");
         break;
     case 'M':
+    case 'm':
         printf("mad bull.\n");
         break;
     case 'N':
+    case 'n':
         printf("native soldiers.\n");
         break;
     case 'O':
+    case 'o':
         printf("obedient student.\n");
         break;
     case 'P':
+    case 'p':
         printf("playful panda.\n");
         break;
     case 'Q':
+    case 'q':
         printf("quarrelsome people.\n");
         break;
     case 'R':
+    case 'r':
         printf("rapid spin.\n");
         break;
     case 'S':
+    case 's':
         printf("sad face.\n");
         break;
     case 'T':
+    case 't':
         printf("tricky question.\n");
         break;
     case 'U':
+    case 'u':
         printf("ugly cockroach.\n");
         break;
     case 'V':
+    case 'v':
         printf("virtual classroom.\n");
         break;
     case 'W':
+    case 'w':
         printf("warm climate.\n");
         break;
     case 'X':
+    case 'x':
         printf("xerophytic plant.\n");
         break;
     case 'Y':
+    case 'y':
         printf("yellow color.\n");
         break;
     case 'Z':
+    case 'z':
         printf("zigzag road.\n");
         break;
     default:
         printf("invalid.\n");
 
     }
+    switch(second){
+    case 'A':
+    case 'a':
+        printf("aged actor.\n");
+        break;
+    case 'B':
+    case 'b':
+        printf("big basket.\n");
+        break;
+    case 'C':
+    case 'c':
+        printf("costly car.\n");
+        break;
+    case 'D':
+    case 'd':
+        printf("dark chocolate.\n");
+        break;
+    case 'E':
+    case 'e':
+        printf("edible egg.\n");
+        break;
+    case 'F':
+    case 'f':
+        printf("fast food.\n");
+        break;
+    case 'G':
+    case 'g':
+        printf("glamorous.\n");
+        break;
+    case 'H':
+    case 'h':
+        printf("haunted house.\n");
+        break;
+    case 'I':
+    case 'i':
+        printf("icy cool.\n");
+        break;
+    case 'J':
+    case 'j':
+        printf("jealous courtiers.\n");
+        break;
+    case 'K':
+    case 'k':
+        printf("keen eyes.\n");
+        break;
+    case 'L':
+    case 'l':
+        printf("last warning.\n");
+        break;
+    case 'M':
+    case 'm':
+        printf("mad bull.\n");
+        break;
+    case 'N':
+    case 'n':
+        printf("native soldiers.\n");
+        break;
+    case 'O':
+    case 'o':
+        printf("obedient student.\n");
+        break;
+    case 'P':
+    case 'p':
+        printf("playful panda.\n");
+        break;
+    case 'Q':
+    case 'q':
+        printf("quarrelsome people.\n");
+        break;
+    case 'R':
+    case 'r':
+        printf("rapid spin.\n");
+        break;
+    case 'S':
+    case 's':
+        printf("sad face.\n");
+        break;
+    case 'T':
+    case 't':
+        printf("tricky question.\n");
+        break;
+    case 'U':
+    case 'u':
+        printf("ugly cockroach.\n");
+        break;
+    case 'V':
+    case 'v':
+        printf("virtual classroom.\n");
+        break;
+    case 'W':
+    case 'w':
+        printf("warm climate.\n");
+        break;
+    case 'X':
+    case 'x':
+        printf("xerophytic plant.\n");
+        break;
+    case 'Y':
+    case 'y':
+        printf("yellow color.\n");
+        break;
+    case 'Z':
+    case 'z':
+        printf("zigzag road.\n");
+        break;
+    default:
+        printf("invalid.\n");
+
+    }
+
     return 0;
 }
